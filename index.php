ba4c9b4bcaa979986c37987c3545c175123e47be
Mahmoud Reyad <theghostspy1@gmail.com>
2018-02-15 01:47:35 +0200

    Adding Bootstrap NavBar

--- index.php
+++ index.php
@@ -7,11 +7,30 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto+Slab" >
         <link rel="stylesheet" href="css//style.css">
     </head>
     <body>
-      
+        <!--Start NavBar  -->
+        <nav class="navbar navbar-default">
+          <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle ="collapse" data-target = "#mynavbar"
+                 aria-expanded = "false">
+                 <span class="sr-only">toggle navigation</span>
+                 <span class="icon-bar"></span>
+                 <span class="icon-bar"></span>
+                 <span class="icon-bar"></span>
+               </button>
+               <a class="navbar-brand" href="#">Login System </a>
+            </div>
+            <div class="collapse navbar-collapse" id="mynavbar">
+              <ul class="nav navbar-nav navbar-right">
+              <li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
+              <li><a href="#">Link</a></li>
+            </div>
+          </div>
+        </nav>
+        <!--End NabBar  -->
       <script src="http://code.jquery.com/jquery-3.3.1.min.js"></script>
       <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
       <script src="js/custom.js"></script>
