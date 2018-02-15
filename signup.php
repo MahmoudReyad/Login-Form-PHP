8f16344fad30536f392237ee44a35ded107b3911
Mahmoud Reyad <theghostspy1@gmail.com>
2018-02-15 20:42:36 +0200

    initial commit

--- signup.php
+++ signup.php
@@ -3,11 +3,12 @@
   <div class="container signup-container">
     <div class="row">
       <div class="col-xs-12">
-        <form class="signup-form" method="POST" action="includes/signup.inc.phpph">
+
+        <form class="signup-form" method="POST" action="includes/signup.inc.php">
           <input type="text" class="form-control" name="user_firstname" placeholder="First Name" required>
           <input type="text" class="form-control" name="user_lastname" placeholder="Lasst Name" required>
-          <input type="text" class="form-control" name="user_lastname" placeholder="User name" required>
-          <input type="email" class="form-control" name="user_firstname" placeholder="E-mail" required>
+          <input type="text" class="form-control" name="email" placeholder="User name" required>
+          <input type="email" class="form-control" name="email" placeholder="E-mail" required>
           <input type="password" class="form-control" name="pwd" placeholder="Password" required>
           <input type="submit" class="form-control btn btn-success btn-block" name="signup-submission">
         </form>
